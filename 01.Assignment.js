93b5f302ff67a6852c49423264f903045db3ae52
Ankita Fartade <ankitafartde.work@gmail.com>
2025-09-24 15:04:17 +0530

    assignment

---
+++ 01.Assignment.js
@@ -0,0 +1,79 @@
+// Write a single line comment which says, comments can make code readable
+//comments can make code readable
+
+// Write another signgle comment which says, Welcome to 30DaysOfJavaScript
+//Welcome to 30DaysOfJavaScript
+
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/*
+comments can make code readable,
+easy to reuse and informative
+*/
+
+// Declare four variables without assigning values
+let a;
+let n;
+let k;
+let i;
+console.log (a);
+console.log (n);
+console.log (k);
+console.log (i);
+
+// Declare four variables with assigned values
+let f = 15;
+let b = "Hii";
+let c = true;
+let d = 2.24;
+console.log (f);
+console.log (b);
+console.log (c);
+console.log (d);
+
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let fName = "Ankita";
+let lName = "Fartade";
+let mStatus = "Single";
+let country1 = "India";
+let age1 = "23";
+console.log (fName);
+console.log (lName);
+console.log (maStatus);
+console.log (country1);
+console.log (age1);
+
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+let firstName = "Ankita", lastName="Fartade", maritalStatus="Single", age="23` ", country="India";
+console.log (firstName);
+console.log (lastName);
+console.log (maritalStatus);
+console.log (age);
+console.log (country);
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the console.
+let myAge = "23";
+let yourAge = "26";
+console.log (myAge);
+console.log (yourAge);
+
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+//variable.js
+
+//string
+let myName="Ankita";
+
+//boolaen
+let isStudent= true;
+
+//undefined
+let=notAssigned
+
+//null
+let x= null;
+
+
+
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+// completed in the above question.
